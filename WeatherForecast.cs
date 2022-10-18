faa75afc3b39b2c3e1a541a556408393ef90db0c
hunter hart <hjhart1@crimson.ua.edu>
2022-10-18 12:23:34 -0500

    api first

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace mis321___pa3_hjhart1;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
